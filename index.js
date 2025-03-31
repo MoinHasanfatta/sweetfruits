5362a4cbccacbbbb2c2d612c7f4125986a595845
Moin Hasanfatta <hasanfattamoin@gmail.com>
2025-03-31 21:26:08 +0530

    fruits

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
+app.use(cors())
 
 const fruits = [
   { id: 1, name: 'Apple', color: 'Red', taste: 'Sweet' },
